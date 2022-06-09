dd3b2822ab270e8b7e92d5cb9105fa0e7bb08cf4
Bohdan Tsehelnyk <mail.qewby@gmail.com>
2022-06-09 15:31:01 +0300

    feat: create Maven project

---
+++ src/test/java/com/qewby/network/AppTest.java
@@ -0,0 +1,12 @@
+package com.qewby.network;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class AppTest {
+
+    @Test
+    public void testApp() {
+        assertEquals(App.helloWorld(), "Hello world");
+    }
+}
