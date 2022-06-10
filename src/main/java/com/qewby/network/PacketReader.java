0207240e80867d569792c696f58d237b75a5e3ac
Bohdan Tsehelnyk <mail.qewby@gmail.com>
2022-06-10 23:40:59 +0300

    feat: implement PacketReader without decryption

---
+++ src/main/java/com/qewby/network/PacketReader.java
@@ -0,0 +1,41 @@
+package com.qewby.network;
+
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+
+public class PacketReader {
+
+    public static Packet read(final byte[] packet) throws IllegalArgumentException {
+        ByteBuffer buffer = ByteBuffer.wrap(packet);
+        int startOffset = 0;
+
+        final byte bMagic = buffer.get();
+        if (bMagic != Packet.MagicValue) {
+            throw new IllegalArgumentException("Bad magic byte value");
+        }
+        final byte bSrc = buffer.get();
+        final long bPktId = buffer.getLong();
+        final int wLen = buffer.getInt();
+        final short wCrc16 = buffer.getShort();
+        short expectedCrc16 = CRC16
+                .calculate(Arrays.copyOfRange(packet, startOffset, buffer.position() - Short.BYTES));
+        if (wCrc16 != expectedCrc16) {
+            throw new IllegalArgumentException("Invalid packet header: CRC16 not valid");
+        }
+        startOffset = buffer.position();
+
+        final int cType = buffer.getInt();
+        final int bUserId = buffer.getInt();
+        final byte[] message = new byte[wLen - Integer.BYTES * 2];
+        buffer.get(message, 0, wLen - Integer.BYTES * 2);
+        final short wMsgCrc16 = buffer.getShort();
+        expectedCrc16 = CRC16
+                .calculate(Arrays.copyOfRange(packet, startOffset, buffer.position() - Short.BYTES));
+        if (wMsgCrc16 != expectedCrc16) {
+            throw new IllegalArgumentException("Invalid packet header: CRC16 not valid");
+        }
+
+        final Message bMsg = new Message(cType, bUserId, message);
+        return new Packet(bMagic, bSrc, bPktId, wLen, wCrc16, bMsg, wMsgCrc16);
+    }
+}
---
+++ src/test/java/com/qewby/network/PacketReaderTest.java
@@ -0,0 +1,103 @@
+package com.qewby.network;
+
+import static org.junit.Assert.assertEquals;
+
+import java.nio.BufferUnderflowException;
+import java.util.Arrays;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PacketReaderTest {
+    private byte[] validPacket;
+
+    @Test
+    public void testDefaultConstructor() {
+        new PacketReader();
+    }
+
+    @Before
+    public void createPacketByteArray() {
+        validPacket = new byte[] {
+                0x13,
+                0x01,
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0f,
+                0x00, 0x00, 0x00, 0x0A,
+                (byte) 0x54, (byte) 0xC4, // CRC16
+                0x00, 0x00, 0x00, 0x01,
+                0x00, 0x00, 0x00, 0x01,
+                (byte) 'O', (byte) 'K',
+                0x07, 0x35 // CRC16
+        };
+    }
+
+    @Test
+    public void testHeaderCRC16() {
+        short expected = (short) 0x54C4;
+        short actual = CRC16.calculate(Arrays.copyOfRange(validPacket, 0, 14));
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testMessageCRC16() {
+        short expected = (short) 0x0735;
+        short actual = CRC16.calculate(Arrays.copyOfRange(validPacket, 16, 26));
+        assertEquals(expected, actual);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testInvalidMagicNumber() {
+
+        byte[] packet = validPacket.clone();
+        packet[0] = 0;
+        PacketReader.read(packet);
+    }
+
+    @Test(expected = BufferUnderflowException.class)
+    public void testBufferUnderflow() {
+        byte[] packet = Arrays.copyOf(validPacket, 10);
+        PacketReader.read(packet);
+    }
+
+    @Test(expected = BufferUnderflowException.class)
+    public void testValidHeaderCRC16() {
+        byte[] packet = Arrays.copyOf(validPacket, 17);
+        PacketReader.read(packet);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testInvalidHeaderCRC16() {
+        byte[] packet = Arrays.copyOf(validPacket, 17);
+        ++(packet[14]);
+        PacketReader.read(packet);
+    }
+
+    @Test(expected = BufferUnderflowException.class)
+    public void testShorterMessageLength() {
+        byte[] packet = Arrays.copyOf(validPacket, validPacket.length - 1);
+        PacketReader.read(packet);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testLongerMessageLength() {
+        byte[] packet = new byte[validPacket.length + 1];
+        System.arraycopy(validPacket, 0, packet, 0, validPacket.length);
+        packet[packet.length - 1] = packet[packet.length - 2]; // CRC part
+        packet[packet.length - 2] = packet[packet.length - 3]; // CRC part
+        packet[packet.length - 3] = (byte) '!';
+        PacketReader.read(packet);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testInvalidMessageCRC16() {
+        byte[] packet = validPacket.clone();
+        ++(packet[packet.length - 1]);
+        PacketReader.read(packet);
+    }
+
+    @Test
+    public void testValidPacket() {
+        byte[] packet = validPacket.clone();
+        PacketReader.read(packet);
+    }
+}
