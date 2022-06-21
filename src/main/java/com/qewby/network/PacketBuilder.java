8e35734cd617fe4290126c3f8d8d26550166756b
Bohdan Tsehelnyk <mail.qewby@gmail.com>
2022-06-21 17:40:29 +0300

    feat: create PackageBuilder to build Pakage object from Message

---
+++ src/main/java/com/qewby/network/PacketBuilder.java
@@ -0,0 +1,21 @@
+package com.qewby.network;
+
+import java.nio.ByteBuffer;
+
+public class PacketBuilder {
+    public static Packet build(Message message) {
+        byte bMagic = Packet.MagicValue;
+        byte bSrc = 111;
+        long bPktId = 1;
+        int wLen = message.getMessage().length + Integer.BYTES * 2;
+        ByteBuffer header = ByteBuffer.allocate(14);
+        header.put(bMagic).put(bSrc).putLong(bPktId).putInt(wLen);
+        short wCrc16 = CRC16.calculate(header.array());
+
+        ByteBuffer msg = ByteBuffer.allocate(wLen);
+        msg.putInt(message.getCType()).putInt(message.getBUserId()).put(message.getMessage());
+        short wMsgCrc16 = CRC16.calculate(msg.array());
+        Packet result = new Packet(bMagic, bSrc, bPktId, wLen, wCrc16, message, wMsgCrc16);
+        return result;
+    }
+}
---
+++ src/test/java/com/qewby/network/PacketBuilderTest.java
@@ -0,0 +1,46 @@
+package com.qewby.network;
+
+import static org.junit.Assert.assertEquals;
+
+import java.nio.ByteBuffer;
+
+import org.junit.Test;
+
+public class PacketBuilderTest {
+    private final Message message = new Message(1, 1, "Hello world".getBytes());
+
+    @Test
+    public void testDefaultConstructor() {
+        new PacketBuilder();
+    }
+
+    @Test
+    public void testWLenValueEqualsToMessageSize() {
+        Packet actual = PacketBuilder.build(message);
+        assertEquals(message.getMessage().length + Integer.BYTES * 2, actual.getWLen());
+    }
+
+    @Test
+    public void testMagicNumberIsValid() {
+        Packet actual = PacketBuilder.build(message);
+        assertEquals(Packet.MagicValue, actual.getBMagic());
+    }
+
+    @Test
+    public void testMessageInPackageEqualsToInitialMessage() {
+        Packet actual = PacketBuilder.build(message);
+        assertEquals(message.getCType(), actual.getBMsg().getCType());
+        assertEquals(message.getBUserId(), actual.getBMsg().getBUserId());
+        assertEquals(message.getMessage(), actual.getBMsg().getMessage());
+    }
+
+    @Test
+    public void testMessageCRC16IsValid() {
+        Packet packet = PacketBuilder.build(message);
+        ByteBuffer msg = ByteBuffer.allocate(message.getMessage().length + Integer.BYTES * 2);
+        msg.putInt(message.getCType()).putInt(message.getBUserId()).put(message.getMessage());
+        short expected = CRC16.calculate(msg.array());
+        short actual = packet.getWMsgCrc16();
+        assertEquals(expected, actual);
+    }
+}
