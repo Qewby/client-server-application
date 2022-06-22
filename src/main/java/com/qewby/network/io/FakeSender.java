5ec9373ee26810b31a483fe6cd3eb6600c8319cd
Bohdan Tsehelnyk <mail.qewby@gmail.com>
2022-06-22 19:40:58 +0300

    refactor: replace sout to logger

--- src/main/java/com/qewby/network/io/FakeSender.java
+++ src/main/java/com/qewby/network/io/FakeSender.java
@@ -3,12 +3,13 @@ package com.qewby.network.io;
 import java.lang.String;
 
 import java.net.InetAddress;
+import java.util.logging.Logger;
 
 public class FakeSender implements Sender {
-
+    private static Logger logger = Logger.getGlobal();
     @Override
     public void sendMessage(byte[] packet, InetAddress target) {
         String packetString = new String(packet);
-        System.out.println(packetString);
+        logger.info("Sended message: " + packetString);
     }
 }
