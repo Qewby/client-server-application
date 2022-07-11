ae2d9b3d89258a59d94b065cedf93546e7b3c112
volkov <volkov.ivan08.12@gmail.com>
2022-07-11 16:37:54 +0300

    add service functionality for grups + controller and junit test using assert j.

--- src/main/java/com/qewby/network/controller/GroupController.java
+++ src/main/java/com/qewby/network/controller/GroupController.java
@@ -2,12 +2,16 @@ package com.qewby.network.controller;
 
 import java.util.List;
 
+import com.qewby.network.annotation.RequestBody;
 import com.qewby.network.annotation.RequestMapping;
 import com.qewby.network.annotation.RequestMethod;
+import com.qewby.network.annotation.RequestParameter;
 import com.qewby.network.annotation.RestController;
 import com.qewby.network.dto.GroupDto;
 import com.qewby.network.service.GroupService;
 import com.qewby.network.service.implementation.DefaultGroupService;
+import com.sun.net.httpserver.Authenticator;
+
 
 @RestController
 public class GroupController {
@@ -19,4 +23,30 @@ public class GroupController {
         return groupService.getAllGroups();
     }
 
+	@RequestMapping(path = "/api/group", method = RequestMethod.PUT)
+	public GroupDto addGroup(@RequestBody GroupDto groupDto) {
+		if (groupService.addGroup(groupDto) == 1)
+		{
+			return groupDto;
+		}
+		return null;
+	}
+
+	@RequestMapping(path = "/api/group", method = RequestMethod.POST)
+	public GroupDto updateGroup(@RequestBody GroupDto groupDto) {
+		if (groupService.updateGroup(groupDto) == 1)
+		{
+			return groupDto;
+		}
+		return null;
+	}
+
+	@RequestMapping(path = "/api/group", method = RequestMethod.DELETE)
+	public Integer deleteGroup(@RequestParameter("id") Integer groupId) {
+		if (groupService.deleteGroupById(groupId) == 1)
+		{
+			return groupId;
+		}
+		return null;
+	}
 }
--- src/main/java/com/qewby/network/service/GroupService.java
+++ src/main/java/com/qewby/network/service/GroupService.java
@@ -4,6 +4,18 @@ import java.util.List;
 
 import com.qewby.network.dto.GroupDto;
 
-public interface GroupService {
-    List<GroupDto> getAllGroups();
+
+public interface GroupService
+{
+	List<GroupDto> getAllGroups();
+
+	int addGroup(GroupDto groupDto);
+
+	int updateGroup(GroupDto groupDto);
+
+	int deleteGroupById(Integer id);
+
+	GroupDto getGroupById(Integer id);
+
+	GroupDto getGroupByName(String name);
 }
